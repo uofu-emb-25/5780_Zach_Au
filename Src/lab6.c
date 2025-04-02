7968f0cdf3fbeae95f6b9809b9fe6a46bf7981c5
auz21 <zachouch68@gmail.com>
2025-04-02 10:21:02 -0600

    worked on 6.2

--- Src/lab6.c
+++ Src/lab6.c
@@ -4,6 +4,10 @@
 
 void SystemClock_Config(void);
 
+// Sine Wave: 8-bit, 32 samples/cycle
+const uint8_t sine_table[32] = {127,151,175,197,216,232,244,251,254,251,244,
+    232,216,197,175,151,127,102,78,56,37,21,9,2,0,2,9,21,37,56,78,102};    
+
 int lab6_main(void) {
     HAL_Init();
 
@@ -17,14 +21,26 @@ int lab6_main(void) {
     // Enable ADC1 clock
     RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
 
+    // Enable APB1 clock
+    RCC->APB1ENR |= RCC_APB1ENR_DACEN; 
+
     ADC1->CFGR1 &= ~ADC_CFGR1_RES; // 8-bit resolution (00)
     ADC1->CFGR1 |= ADC_CFGR1_CONT; // Continuous conversion
     ADC1->CFGR1 &= ~ADC_CFGR1_EXTEN; // Software trigger
 
     ADC1->CHSELR = ADC_CHSELR_CHSEL0; // Select channel 0 (PA0) for ADC Input
 
-    GPIOA->MODER |= (3 << (2 * 0)); // Set PA0 to analog mode (11)
+    GPIOA->MODER |= (3 << (2 * 0)); // Set PA0 to analog mode
     GPIOA->PUPDR &= ~(3 << (2 * 0)); // No pull-up/pull-down
+
+    GPIOA->MODER |= (3 << (2 * 4));  // Set PA4 to analog mode
+    GPIOA->PUPDR &= ~(3 << (2 * 4)); // No pull-up/pull-down
+
+    // Set up DAC channel
+    DAC->CR &= ~DAC_CR_EN1;           // Disable DAC channel 1
+    DAC->CR &= ~DAC_CR_TSEL1;         // Select software trigger (000)
+    DAC->CR |= DAC_CR_TEN1;           // Enable trigger
+    DAC->CR |= DAC_CR_EN1;            // Enable DAC channel 1
 
     // Start Calibration
     // Ensure ADEN is 0
@@ -48,20 +64,26 @@ int lab6_main(void) {
 
     ADC1->CR |= ADC_CR_ADSTART;
 
+    uint8_t i = 0;
     // Infinite While Loop
     while(1) {
-        // Wait until ADC conversion is complete
-        if (ADC1->ISR & ADC_ISR_EOC) {
-            uint8_t adc_value = ADC1->DR; // Read 8-bit ADC value
+        // // Wait until ADC conversion is complete
+        // if (ADC1->ISR & ADC_ISR_EOC) {
+        //     uint8_t adc_value = ADC1->DR; // Read 8-bit ADC value
 
-            // Clear all LED bits first (PC6 to PC9)
-            GPIOC->ODR &= ~((0xF) << 6); // Clear PC6-PC9
+        //     // Clear all LED bits first (PC6 to PC9)
+        //     GPIOC->ODR &= ~((0xF) << 6); // Clear PC6-PC9
 
-            // Set LEDs based on increasing ADC thresholds (note the potentimeter that I am using is a 104)
-            if (adc_value > 64) My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET); // RED LED on
-            if (adc_value > 128) My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET); // BLUE LED on
-            if (adc_value > 192) My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET); // ORANGE LED on
-            if (adc_value > 224) My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET); // GREEN LED on
-        }
+        //     // Set LEDs based on increasing ADC thresholds (note the potentimeter that I am using is a 104)
+        //     // Also, the leds will cycle back to "0" for some reason, could be the potentimeter
+        //     if (adc_value > 64) My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET); // RED LED on
+        //     if (adc_value > 128) My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET); // BLUE LED on
+        //     if (adc_value > 192) My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET); // ORANGE LED on
+        //     if (adc_value > 224) My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET); // GREEN LED on
+        // }
+
+        DAC->DHR8R1 = sine_table[i];   // Write 8-bit value to DAC channel 1
+        i = (i + 1) % 32;             // Loop through 32-sample waveform
+        delay_ms(1);                  // 1ms delay = ~31Hz waveform
     }
 }
